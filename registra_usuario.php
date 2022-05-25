907f22ba1969514de962c0cd21c8564a1e841708
tiago <tiagolemespalhano@gmail.com>
2022-05-25 18:01:34 -0300

    212. Registrando usuários parte 2 - Registrando dados no MySQL

--- registra_usuario.php
+++ registra_usuario.php
@@ -1,10 +1,25 @@
 <?php
 
+	require_once('db.class.php');
+
 	$usuario = $_POST['usuario'];
 	$email = $_POST['email'];
 	$senha = $_POST['senha'];
 
+	$obj_db = new db();
+
+	$link = $obj_db->conecta_mysql();
+
+	$sql = "INSERT INTO `usuarios` (`id`, `nome`, `senha`, `email`) VALUES (NULL, '$usuario', '$senha', '$email');";
 	
+	 if (mysqli_query($link, $sql)) {
+	 	echo 'succes';
+	 }
+	 else {
+	 	echo 'failed';
+	 }
+
+
 
 
 ?>
